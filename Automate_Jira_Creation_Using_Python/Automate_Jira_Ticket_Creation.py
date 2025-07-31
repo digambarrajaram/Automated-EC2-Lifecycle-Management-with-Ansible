e10530449cdba4b687e14a2e3968289cdb96a014
root <root@LAPTOP-9AOS1NLD.localdomain>
2025-07-31 09:57:02 +0000

    Ansible-Galaxy-Deployment-Test

--- Automate_Jira_Creation_Using_Python/Automate_Jira_Ticket_Creation.py
+++ Automate_Jira_Creation_Using_Python/Automate_Jira_Ticket_Creation.py
@@ -4,12 +4,10 @@ import requests
 from requests.auth import HTTPBasicAuth
 import json
 
-url = "https://digambarrajaram2.atlassian.net/rest/api/3/issue"
-
-EMAIL = "digambarrajaram2@gmail.com"
-API_TOKEN = "ATATT3xFfGF0t1dR4XNToaUsuyVyUsq3-ZUM1wgmRWnOEuA8d7vL4nUkPppTHYYLtAvZdtGHaezjxIM1NX-JnUxDcHJngxnQLb_ki4NujniLbuKh-EmEUBT1081CvN4Js8Ss7UDM8XeZhogvmmc_FWHMnK0e88J9oTxx9TvMcnKaHVz3Q-ZIU6Q=F44D34E7"
-
-auth = HTTPBasicAuth(EMAIL, API_TOKEN)
+url = ""
+EMAIl = ""
+API_TOKEN = ""
+auth = HTTPBasicAuth(EMAIl, API_TOKEN)
 
 headers = {
   "Accept": "application/json",
@@ -18,13 +16,12 @@ headers = {
 
 payload = json.dumps( {
   "fields": {
-
     "description": {
       "content": [
         {
           "content": [
             {
-              "text": "My first JIRA Ticket",
+              "text": "First jira ticket automation",
               "type": "text"
             }
           ],
@@ -40,7 +37,7 @@ payload = json.dumps( {
     "project": {
       "key": "SCRUM"
     },
-    "summary": "First Jira Ticket",
+    "summary": "First jira ticket automation",
   },
   "update": {}
 } )
